795854a43e2c18a6b105604ee63313a16ff281d7
Gregory Jacobs <greg@greg-jacobs.com>
2013-06-10 11:22:00 -0400

    Add integration tests for Model with Float and Integer attributes.

---
+++ tests/spec/integration/Model_with_FloatAttribute.js
@@ -0,0 +1,39 @@
+/*global define, window, _, describe, beforeEach, afterEach, it, expect, JsMockito */
+define( [
+	'data/Model',
+	'data/attribute/Float'
+], function( Model ) {
+	
+	describe( "Integration: Model with Float Attribute", function() {
+		
+		it( "The attribute should have a default value of 0, if `useNull` is false", function() {
+			var MyModel = Model.extend( {
+				attributes : [
+					{ name : 'floatAttr', type: 'float' }
+				]
+			} );
+
+			var model = new MyModel();
+			expect( model.get( 'floatAttr' ) ).toBe( 0 );
+			
+			var model2 = new MyModel( {} );
+			expect( model2.get( 'floatAttr' ) ).toBe( 0 );
+		} );
+		
+		
+		it( "The attribute should have a default value of `null`, if `useNull` is true", function() {
+			var MyModel = Model.extend( {
+				attributes : [
+					{ name : 'floatAttr', type: 'float', useNull: true }
+				]
+			} );
+			
+			var model = new MyModel();
+			expect( model.get( 'floatAttr' ) ).toBe( null );
+			
+			var model2 = new MyModel( {} );
+			expect( model2.get( 'floatAttr' ) ).toBe( null );
+		} );
+		
+	} );
+} );
